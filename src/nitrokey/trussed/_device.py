4f794aef9619b54333d69b64f7b9ce339702801c
Sosthène Guédon <sosthene@nitrokey.com>
2025-11-24 16:51:21 +0100

    Forward command through ccid

--- src/nitrokey/trussed/_device.py
+++ src/nitrokey/trussed/_device.py
@@ -19,7 +19,7 @@ from smartcard.Exceptions import NoCardException
 from smartcard.System import readers
 
 from ._base import TrussedBase
-from ._utils import Fido2Certs, Uuid
+from ._utils import Fido2Certs, Iso7816Apdu, Uuid
 
 T = TypeVar("T", bound="TrussedDevice")
 
@@ -33,6 +33,14 @@ class App(Enum):
     SECRETS = 0x70
     PROVISIONER = 0x71
     ADMIN = 0x72
+
+    def aid(self) -> bytes:
+        if self == App.SECRETS:
+            return bytes.fromhex("A000000527 2101")
+        elif self == App.ADMIN:
+            return bytes.fromhex("A00000084700000001")
+        elif self == App.PROVISIONER:
+            return bytes.fromhex("A00000084700000001")
 
 
 class TrussedDevice(TrussedBase):
@@ -75,18 +83,18 @@ class TrussedDevice(TrussedBase):
         if isinstance(self.device, CtapHidDevice):
             self.device.wink()
 
-    def _call(
+    def _call_admin_legacy(
         self,
         command: int,
         command_name: str,
         response_len: Optional[int] = None,
         data: bytes = b"",
     ) -> bytes:
-        response  =  []
+        response: bytes = bytes()
         if isinstance(self.device, CtapHidDevice):
             response = self.device.call(command, data=data)
         else:
-            response = _call_ccid(self.device, command, data)
+            response = self._call_admin_ccid_legacy(command, data)
 
         if response_len is not None and response_len != len(response):
             raise ValueError(
@@ -95,10 +103,86 @@ class TrussedDevice(TrussedBase):
             )
         return response
 
+    def _call_admin_ccid_legacy(
+        self, command: int, data: bytes, response_len: Optional[int] = None
+    ) -> bytes:
+        assert not isinstance(self.device, CtapHidDevice)
+        app = App.ADMIN
+        select = bytes([0xA0, 0xA4, 0x04, 0x00, len(app.aid())]) + app.aid()
+        data, sw1, sw2 = self.device.transmit(select)
+        if sw1 != 0x90 or sw2 != 0x00:
+            raise ValueError(
+                f"Failed to select application {app}, got error code: {hex(sw1 << 8 | sw2)}"
+            )
+        p1 = 0
+        if len(data) == 1:
+            p1 = data[0]
+            data = bytes()
+        apdu = Iso7816Apdu(0xA0, command, 0, p1, data, le=response_len)
+        data, sw1, sw2 = self.device.transmit(apdu.to_bytes())
+        accumulator = data
+        while True:
+            if sw1 == 0x61:
+                data, sw1, sw2 = self.device.transmit(
+                    Iso7816Apdu(0x00, 0xC0, 0, 0, None, sw2).to_bytes()
+                )
+                continue
+
+            if sw1 != 0x90 or sw2 != 0x00:
+                raise ValueError(
+                    f"Failed to run command, got error code: {hex(sw1 << 8 | sw2)}"
+                )
+            accumulator += data
+        return accumulator
+
+    def _call_ccid(
+        self,
+        app: App,
+        response_len: Optional[int] = None,
+        data: bytes = b"",
+    ) -> bytes:
+        assert not isinstance(self.device, CtapHidDevice)
+        select = bytes([0xA0, 0xA4, 0x04, 0x00, len(app.aid())]) + app.aid()
+        data, sw1, sw2 = self.device.transmit(select)
+        if sw1 != 0x90 or sw2 != 0x00:
+            raise ValueError(
+                f"Failed to select application {app}, got error code: {hex(sw1 << 8 | sw2)}"
+            )
+
+        command = Iso7816Apdu(0xA0, app.value, 0, 0, data, le=response_len)
+
+        data, sw1, sw2 = self.device.transmit(command.to_bytes())
+        accumulator = data
+        while True:
+            if sw1 == 0x61:
+                data, sw1, sw2 = self.device.transmit(
+                    Iso7816Apdu(0x00, 0xC0, 0, 0, None, sw2).to_bytes()
+                )
+                continue
+
+            if sw1 != 0x90 or sw2 != 0x00:
+                raise ValueError(
+                    f"Failed to run command, got error code: {hex(sw1 << 8 | sw2)}"
+                )
+            accumulator += data
+        return accumulator
+
     def _call_app(
         self, app: App, response_len: Optional[int] = None, data: bytes = b""
     ) -> bytes:
-        return self._call(app.value, app.name, response_len, data)
+        response: bytes = bytes()
+        if isinstance(self.device, CtapHidDevice):
+            response = self.device.call(app.value, data=data)
+        else:
+            response = self._call_ccid(app, response_len, data)
+
+        if response_len is not None and response_len != len(response):
+            raise ValueError(
+                f"The response for the CTAPHID {app.name} command has an unexpected length "
+                f"(expected: {response_len}, actual: {len(response)})"
+            )
+        return response
+        # return self._call(app.value, app.name, response_len, data)
 
     @classmethod
     @abstractmethod
@@ -153,15 +237,6 @@ class TrussedDevice(TrussedBase):
             devices.append(cls.from_device(connection))
 
         return devices
-
-
-def _call_ccid(
-    device: CardConnection,
-    command: int,
-    data: bytes = b"",
-    response_len: Optional[int] = None,
-) -> bytes:
-    raise NotImplementedError("TODO")
 
 
 def _device_path_to_str(path: Union[bytes, str]) -> str:
--- src/nitrokey/trussed/admin_app.py
+++ src/nitrokey/trussed/admin_app.py
@@ -198,11 +198,14 @@ class AdminApp:
         self.device = device
 
     def _call(
-        self, command: AdminCommand, response_len: Optional[int] = None, data: bytes = b""
+        self,
+        command: AdminCommand,
+        response_len: Optional[int] = None,
+        data: bytes = b"",
     ) -> Optional[bytes]:
         try:
             if command.is_legacy_command():
-                return self.device._call(
+                return self.device._call_admin_legacy(
                     command.value, command.name, response_len=response_len, data=data
                 )
             else:
@@ -309,9 +312,15 @@ class AdminApp:
         if not reply:
             return [
                 ConfigField(
-                    "fido.disable_skip_up_timeout", False, False, False, ConfigFieldType.BOOL
+                    "fido.disable_skip_up_timeout",
+                    False,
+                    False,
+                    False,
+                    ConfigFieldType.BOOL,
                 ),
-                ConfigField("opcard.use_se050_backend", True, True, True, ConfigFieldType.BOOL),
+                ConfigField(
+                    "opcard.use_se050_backend", True, True, True, ConfigFieldType.BOOL
+                ),
             ]
         parsed = cbor.decode(reply)
         assert isinstance(parsed, list)
@@ -340,8 +349,12 @@ class AdminApp:
 
     def factory_reset_app(self, application: str) -> bool:
         reply = self._call(
-            AdminCommand.FACTORY_RESET_APP, data=application.encode("ascii"), response_len=1
+            AdminCommand.FACTORY_RESET_APP,
+            data=application.encode("ascii"),
+            response_len=1,
         )
         if reply:
-            FactoryResetStatus.check(reply[0], "Failed to factory reset the application")
+            FactoryResetStatus.check(
+                reply[0], "Failed to factory reset the application"
+            )
         return reply is not None
