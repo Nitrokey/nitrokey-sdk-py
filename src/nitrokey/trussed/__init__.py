b150de8112a525bd9f5546e206bf48afeeaa355a
Sosthène Guédon <sosthene@nitrokey.com>
2025-12-01 11:56:09 +0100

    Allow listing devices through CCID and add should_default_ccid helper

--- src/nitrokey/trussed/__init__.py
+++ src/nitrokey/trussed/__init__.py
@@ -5,6 +5,9 @@
 # http://opensource.org/licenses/MIT>, at your option. This file may not be
 # copied, modified, or distributed except according to those terms.
 
+import ctypes
+import sys
+from importlib.util import find_spec
 from typing import List, Optional
 
 from ._base import Model as Model  # noqa: F401
@@ -23,18 +26,37 @@ from ._utils import Uuid as Uuid  # noqa: F401
 from ._utils import Version as Version  # noqa: F401
 
 
-def list(*, model: Optional[Model] = None) -> List[TrussedBase]:
+def should_default_ccid() -> bool:
+    if find_spec("smartcard") is None:
+        return False
+
+    if sys.platform != "win32" and sys.platform != "cygwin":
+        # Linux or MacOS don't need admin to access with CTAPHID
+        return False
+
+    try:
+        if ctypes.windll.shell32.IsUserAnAdmin():
+            return False
+        else:
+            return True
+    except Exception:
+        return False
+
+
+def list(
+    use_ccid: bool = should_default_ccid(), *, model: Optional[Model] = None
+) -> List[TrussedBase]:
     devices: List[TrussedBase] = []
 
     if model is None or model == Model.NK3:
         from nitrokey import nk3
 
-        devices.extend(nk3.list())
+        devices.extend(nk3.list(use_ccid))
 
     if model is None or model == Model.NKPK:
         from nitrokey import nkpk
 
-        devices.extend(nkpk.list())
+        devices.extend(nkpk.list(use_ccid))
 
     return devices
 
