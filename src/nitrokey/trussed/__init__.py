1d55a4d49096b4ab71134315b5ac209537f43c05
AdityaMitra5102 <adityaarghya0@gmail.com>
2026-06-26 06:56:13 +0530

    Use IPC

--- src/nitrokey/trussed/__init__.py
+++ src/nitrokey/trussed/__init__.py
@@ -25,6 +25,13 @@ from ._utils import Fido2Certs as Fido2Certs
 from ._utils import Uuid as Uuid
 from ._utils import Version as Version
 
+try:
+    from fido2.hid.ipc import IPC_Pipe
+
+    ipc_available = IPC_Pipe.is_pipe_available()
+except ImportError:
+    ipc_available = False
+
 
 def should_default_ccid() -> bool:
     """Helper function to inform whether CCID should be the default communication protocol
@@ -38,7 +45,7 @@ def should_default_ccid() -> bool:
         return False
 
     try:
-        if ctypes.windll.shell32.IsUserAnAdmin():
+        if ctypes.windll.shell32.IsUserAnAdmin() or ipc_available:
             return False
         else:
             return True
